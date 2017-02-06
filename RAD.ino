06970ba5c71c4122a2a9fbe2537813bea83ae5bc
Paul An <anpaul0615@gmail.com>
2017-02-06 16:48:26 +0900

    통신관련 테스트 코드

--- RAD.ino
+++ RAD.ino
@@ -1,10 +1,15 @@
 #include <MemoryFree.h>
 #include <FreeRTOS_AVR.h>
+#include "CommunicationManager.h"
+
 
 /* ******************************************
  * Global
  * ******************************************/
-HardwareSerial* serial_monitor = &Serial;  // RxTx0
+HardwareSerial* serial_monitor = &Serial;  // RXTX0
+HardwareSerial* serial_esp8266 = &Serial2;  // RXTX2
+//CommunicationManager comm;
+//DisplayManager display;
 unsigned int count = 0;
 
 
@@ -38,12 +43,44 @@ void task3(void* arg) {
  * Entry Point
  * ******************************************/
 void setup(void) {
+
+  // 0) 초기화
+  // 장치 기초 정보 
+  // 기본 통신상태 
   serial_monitor->begin(9600);
+  serial_esp8266->begin(115200);
   
+  
+  // 1) UI 쓰레드 (우선순위:1)
+  // 버튼입력 감지
+  // 버튼입력에 따른 화면 그리기
+  // 화면구성에 필요한 데이터 가져오기
+  // ※ 리부팅 선택시, 리부팅함수 직접실행
+  // ※ 통신속도변경 선택시, 통신속더변경함수 직접실행
+  // ※ 승인요청 선택시, 승인요청함수 직접실행 + 승인번호보여주기
+
+  
+  // 2) 통신 쓰레드 (우선순위:1)
+  // 콘솔화면에서만 동작? 백그라운드 동작?
+  // 송수신 병렬처리? 순차처리?
+  // 버퍼초과하면..?
+  
+  
+  // 3) 웹서버 쓰레드
+  // 서버모드화면에서만 동작
+  // 서버모드 선택시, 
+
+  
+  // 4) 로깅 쓰레드
+  // ...
+
+  
+  // 등록
   xTaskCreate( task1, "check free memory", 100, NULL, 1,  NULL );
   xTaskCreate( task2, "print current count", 100, NULL, 1, NULL );
   xTaskCreate( task3, "increase count", 100, NULL, 1, NULL );
   
+  // 시작
   vTaskStartScheduler();
   
   serial_monitor->println( F("[!!!] Insufficient RAM..!") );
